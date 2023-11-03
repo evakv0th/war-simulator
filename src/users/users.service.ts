469d023bf597dd948602130d1f9029a1f44a8dd7
kvothe eva <evakv0th@gmail.com>
2023-11-03 15:23:27 +0200

    fixed ?name= query in users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,8 @@
-import HttpException from "../application/exceptions/http-exceptions";
-import HttpStatusCode from "../application/exceptions/statusCode";
-import { BattleState } from "../application/utils/battle-state";
-import { UserCreateSchema, User } from "../auth/types/auth.interfaces";
-import pool from "../application/db/db";
+import HttpException from '../application/exceptions/http-exceptions';
+import HttpStatusCode from '../application/exceptions/statusCode';
+import { BattleState } from '../application/utils/battle-state';
+import { UserCreateSchema, User } from '../auth/types/auth.interfaces';
+import pool from '../application/db/db';
 
 export async function getUsers(queryParameters?: any): Promise<User[]> {
   const client = await pool.connect();
@@ -10,18 +10,18 @@ export async function getUsers(queryParameters?: any): Promise<User[]> {
   try {
     const { name } = queryParameters;
 
-    let query = "SELECT * FROM users";
+    let query = 'SELECT * FROM users';
     const values = [];
-
     if (name) {
-      query += " WHERE name = $1";
-      values.push(name);
+      query += ' WHERE name ILIKE $1';
+      values.push(`%${name}%`);
     }
+
 
     const result = await client.query(query, values);
     return result.rows;
   } catch (err) {
-    console.error("Database Error:", err);
+    console.error('Database Error:', err);
     throw err;
   } finally {
     client.release();
@@ -32,21 +32,21 @@ export async function getUserById(id: string): Promise<User> {
   const client = await pool.connect();
 
   try {
-    let query = "SELECT * FROM users WHERE id = $1";
+    let query = 'SELECT * FROM users WHERE id = $1';
     const values = [];
     values.push(id);
     const result = await client.query(query, values);
     if (result.rows.length <= 0) {
       throw new HttpException(
         HttpStatusCode.NOT_FOUND,
-        `user with id:${id} not found`
+        `user with id:${id} not found`,
       );
     }
-    let queryArmy = "SELECT * FROM armies WHERE user_id = $1";
+    let queryArmy = 'SELECT * FROM armies WHERE user_id = $1';
     const army = await client.query(queryArmy, [id]);
     return { ...result.rows[0], army: army.rows[0] };
   } catch (err) {
-    console.error("Database Error:", err);
+    console.error('Database Error:', err);
     throw err;
   } finally {
     client.release();
@@ -55,7 +55,7 @@ export async function getUserById(id: string): Promise<User> {
 
 export async function updateUser(
   id: string,
-  updateData: Partial<UserCreateSchema>
+  updateData: Partial<UserCreateSchema>,
 ): Promise<User> {
   const client = await pool.connect();
   const { name, password, email, type } = updateData;
@@ -75,7 +75,7 @@ export async function updateUser(
     arrayWithChanges.push(`type=$${values.push(type)}`);
   }
 
-  const changes = arrayWithChanges.join(", ");
+  const changes = arrayWithChanges.join(', ');
 
   const queryUpd = `UPDATE users SET ${changes} WHERE id=$1 RETURNING *;`;
 
@@ -83,17 +83,17 @@ export async function updateUser(
     if (arrayWithChanges.length === 0) {
       throw new HttpException(
         HttpStatusCode.BAD_REQUEST,
-        "No valid fields provided for update"
+        'No valid fields provided for update',
       );
     }
 
     const result = await client.query(queryUpd, values);
     if (result.rowCount == 0)
-      throw new HttpException(HttpStatusCode.NOT_FOUND, "user not found");
+      throw new HttpException(HttpStatusCode.NOT_FOUND, 'user not found');
 
     return result.rows[0];
   } catch (err) {
-    console.error("Database Error:", err);
+    console.error('Database Error:', err);
     throw err;
   } finally {
     client.release();
@@ -104,13 +104,13 @@ export async function deleteUser(id: string): Promise<User> {
   const client = await pool.connect();
 
   try {
-    let query = "DELETE FROM users WHERE id=$1;";
+    let query = 'DELETE FROM users WHERE id=$1;';
     const values = [];
     values.push(id);
     const result = await client.query(query, values);
     return result.rows[0];
   } catch (err) {
-    console.error("Database Error:", err);
+    console.error('Database Error:', err);
     throw err;
   } finally {
     client.release();
@@ -118,15 +118,15 @@ export async function deleteUser(id: string): Promise<User> {
 }
 
 export async function battle(id: string, enemyId: string) {
-  if (battleTracker.getState() !== "not_started") {
+  if (battleTracker.getState() !== 'not_started') {
     throw new HttpException(
       HttpStatusCode.BAD_REQUEST,
-      "please finish your last battle before starting new one"
+      'please finish your last battle before starting new one',
     );
   } else if (id === enemyId) {
     throw new HttpException(
       HttpStatusCode.BAD_REQUEST,
-      "You cannot fight yourself"
+      'You cannot fight yourself',
     );
   }
   const client = await pool.connect();
@@ -140,7 +140,7 @@ export async function battle(id: string, enemyId: string) {
     ) {
       throw new HttpException(
         HttpStatusCode.BAD_REQUEST,
-        "You cant start battle, you should have at least 1 squad(with weapons!!), 1 tank and 1 plane"
+        'You cant start battle, you should have at least 1 squad(with weapons!!), 1 tank and 1 plane',
       );
     }
     if (
@@ -150,18 +150,18 @@ export async function battle(id: string, enemyId: string) {
     ) {
       throw new HttpException(
         HttpStatusCode.BAD_REQUEST,
-        "You cant start battle, your enemy should have at least 1 tank, 1 plane and 1 squad (with weapons!!)"
+        'You cant start battle, your enemy should have at least 1 tank, 1 plane and 1 squad (with weapons!!)',
       );
     }
     battleTracker.startBattle();
     battleTracker.setEnemyId(enemyId);
     return {
-      msg: "the battle has started! These are stats of you and your enemy (with advantages included in numbers).",
+      msg: 'the battle has started! These are stats of you and your enemy (with advantages included in numbers).',
       msg2: `You can use endpoint /battle/${enemyId}/airBattle to continue the battle and start Air stage.`,
       battleStats: await battleStats,
     };
   } catch (err) {
-    console.error("Database Error:", err);
+    console.error('Database Error:', err);
     throw err;
   } finally {
     client.release();
@@ -170,20 +170,20 @@ export async function battle(id: string, enemyId: string) {
 
 export async function airBattle(id: string, enemyId: string) {
   console.log(battleTracker.getEnemyId());
-  if (battleTracker.getState() !== "in_progress") {
+  if (battleTracker.getState() !== 'in_progress') {
     throw new HttpException(
       HttpStatusCode.BAD_REQUEST,
-      "please start battle first"
+      'please start battle first',
     );
   } else if (battleTracker.getEnemyId() !== enemyId) {
     throw new HttpException(
       HttpStatusCode.BAD_REQUEST,
-      `wrong id of an enemy, you've started battle with user id:${battleTracker.getEnemyId()}`
+      `wrong id of an enemy, you've started battle with user id:${battleTracker.getEnemyId()}`,
     );
   } else if (id === enemyId) {
     throw new HttpException(
       HttpStatusCode.BAD_REQUEST,
-      "You cannot fight yourself"
+      'You cannot fight yourself',
     );
   }
   const client = await pool.connect();
@@ -197,7 +197,7 @@ export async function airBattle(id: string, enemyId: string) {
       battleTracker.winAirBattle();
       return {
         msg: `You won the air battle! Continue battle with /battle/${enemyId}/surfaceBattle`,
-        airBattleResult: "Victory",
+        airBattleResult: 'Victory',
         yourSurfacePlaneStrength:
           battleStats.yourStats.yourPlanesSurfaceStrength,
       };
@@ -209,7 +209,7 @@ export async function airBattle(id: string, enemyId: string) {
       console.log(battleTracker.getState());
       return {
         msg: `Your enemy won the air battle! Continue battle with /battle/${enemyId}/surfaceBattle`,
-        airBattleResult: "Defeat",
+        airBattleResult: 'Defeat',
         enemySurfacePlaneStrength:
           battleStats.enemyStats.enemyPlanesSurfaceStrength,
       };
@@ -217,11 +217,11 @@ export async function airBattle(id: string, enemyId: string) {
       battleTracker.drawAirBattle();
       return {
         msg: `The air battle ended in a draw! Continue battle with /battle/${enemyId}/surfaceBattle`,
-        airBattleResult: "Draw",
+        airBattleResult: 'Draw',
       };
     }
   } catch (err) {
-    console.error("Database Error:", err);
+    console.error('Database Error:', err);
     throw err;
   } finally {
     client.release();
@@ -231,32 +231,32 @@ export async function airBattle(id: string, enemyId: string) {
 export async function surfaceBattle(id: string, enemyId: string) {
   console.log(battleTracker.getState());
   if (
-    battleTracker.getState() !== "air_battle_won" &&
-    battleTracker.getState() !== "air_battle_lost" &&
-    battleTracker.getState() !== "air_draw"
+    battleTracker.getState() !== 'air_battle_won' &&
+    battleTracker.getState() !== 'air_battle_lost' &&
+    battleTracker.getState() !== 'air_draw'
   ) {
     throw new HttpException(
       HttpStatusCode.BAD_REQUEST,
-      "please make air battle first"
+      'please make air battle first',
     );
   } else if (battleTracker.getEnemyId() !== enemyId) {
     throw new HttpException(
       HttpStatusCode.BAD_REQUEST,
-      `wrong id of an enemy, you've started battle with user id:${battleTracker.getEnemyId()}`
+      `wrong id of an enemy, you've started battle with user id:${battleTracker.getEnemyId()}`,
     );
   } else if (id === enemyId) {
     throw new HttpException(
       HttpStatusCode.BAD_REQUEST,
-      "You cannot fight yourself"
+      'You cannot fight yourself',
     );
   }
   const client = await pool.connect();
   try {
     const battleStats = await showStats(id, enemyId);
 
-    if (battleTracker.getState() === "air_battle_won") {
+    if (battleTracker.getState() === 'air_battle_won') {
       battleStats.enemyStats.enemyPlanesSurfaceStrength = 0;
-    } else if (battleTracker.getState() === "air_battle_lost") {
+    } else if (battleTracker.getState() === 'air_battle_lost') {
       battleStats.yourStats.yourPlanesSurfaceStrength = 0;
     } else {
       battleStats.yourStats.yourPlanesSurfaceStrength = 0;
@@ -278,7 +278,7 @@ export async function surfaceBattle(id: string, enemyId: string) {
     } else if (coin <= 0.4) {
       enemyStr *= 1.05;
     }
-    console.log(yourStr, enemyStr, "yours and enemy str for final battle");
+    console.log(yourStr, enemyStr, 'yours and enemy str for final battle');
     if (yourStr > enemyStr) {
       battleTracker.notStarted();
       battleTracker.setEnemyId(null);
@@ -299,7 +299,7 @@ export async function surfaceBattle(id: string, enemyId: string) {
       };
     }
   } catch (err) {
-    console.error("Database Error:", err);
+    console.error('Database Error:', err);
     throw err;
   } finally {
     client.release();
@@ -310,58 +310,58 @@ export async function showStats(id: string, enemyId: string) {
   const client = await pool.connect();
   try {
     const resultUser = await client.query(
-      "SELECT * FROM armies WHERE user_id=$1",
-      [id]
+      'SELECT * FROM armies WHERE user_id=$1',
+      [id],
     );
     if (resultUser.rows.length <= 0) {
       throw new HttpException(
         HttpStatusCode.NOT_FOUND,
-        "you do not have army yet"
+        'you do not have army yet',
       );
     }
     const armyId = resultUser.rows[0].id;
     const resultUserEnemy = await client.query(
-      "SELECT * FROM armies WHERE user_id=$1",
-      [enemyId]
+      'SELECT * FROM armies WHERE user_id=$1',
+      [enemyId],
     );
     if (resultUserEnemy.rows.length <= 0) {
       throw new HttpException(
         HttpStatusCode.NOT_FOUND,
-        "your enemy does not have army yet"
+        'your enemy does not have army yet',
       );
     }
     const armyEnemyId = resultUserEnemy.rows[0].id;
 
     const armyTanksQ = await client.query(
-      "SELECT SUM(strength) FROM tanks WHERE army_id = $1",
-      [armyId]
+      'SELECT SUM(strength) FROM tanks WHERE army_id = $1',
+      [armyId],
     );
     let armyTanksStrength = armyTanksQ.rows[0].sum || 0;
     const armyTanksEnemyQ = await client.query(
-      "SELECT SUM(strength) FROM tanks WHERE army_id = $1",
-      [armyEnemyId]
+      'SELECT SUM(strength) FROM tanks WHERE army_id = $1',
+      [armyEnemyId],
     );
 
     let armyTanksEnemyStrength = armyTanksEnemyQ.rows[0].sum || 0;
-    console.log(armyTanksStrength, armyTanksEnemyStrength, "TANKS STR");
+    console.log(armyTanksStrength, armyTanksEnemyStrength, 'TANKS STR');
     const armyPlanesQAir = await client.query(
-      "SELECT SUM(air_strength) FROM planes WHERE army_id = $1",
-      [armyId]
+      'SELECT SUM(air_strength) FROM planes WHERE army_id = $1',
+      [armyId],
     );
     let armyPlanesStrengthAir = armyPlanesQAir.rows[0].sum || 0;
     const armyPlanesEnemyQAir = await client.query(
-      "SELECT SUM(air_strength) FROM planes WHERE army_id = $1",
-      [armyEnemyId]
+      'SELECT SUM(air_strength) FROM planes WHERE army_id = $1',
+      [armyEnemyId],
     );
     let armyPlanesEnemyStrengthAir = armyPlanesEnemyQAir.rows[0].sum || 0;
     const armyPlanesQSurface = await client.query(
-      "SELECT SUM(surface_strength) FROM planes WHERE army_id = $1",
-      [armyId]
+      'SELECT SUM(surface_strength) FROM planes WHERE army_id = $1',
+      [armyId],
     );
     let armyPlanesStrengthSurface = armyPlanesQSurface.rows[0].sum || 0;
     const armyPlanesEnemyQSurface = await client.query(
-      "SELECT SUM(surface_strength) FROM planes WHERE army_id = $1",
-      [armyEnemyId]
+      'SELECT SUM(surface_strength) FROM planes WHERE army_id = $1',
+      [armyEnemyId],
     );
     let armyPlanesEnemyStrengthSurface =
       armyPlanesEnemyQSurface.rows[0].sum || 0;
@@ -378,35 +378,35 @@ export async function showStats(id: string, enemyId: string) {
     ]);
     let armySquadsEnemyStrength = armySquadsEnemyQ.rows[0].sum || 0;
     switch (resultUser.rows[0].advantage) {
-      case "air":
+      case 'air':
         armyPlanesStrengthAir *= 1.5;
         armyPlanesStrengthSurface *= 1.5;
         break;
-      case "heavy_tech":
+      case 'heavy_tech':
         armyTanksStrength *= 1.5;
         break;
-      case "minefield":
+      case 'minefield':
         armyTanksEnemyStrength *= 0.7;
         armySquadsEnemyStrength *= 0.9;
         break;
-      case "patriotic":
+      case 'patriotic':
         armySquadsStrength *= 1.5;
         break;
     }
 
     switch (resultUserEnemy.rows[0].advantage) {
-      case "air":
+      case 'air':
         armyPlanesEnemyStrengthAir *= 1.5;
         armyPlanesEnemyStrengthSurface *= 1.5;
         break;
-      case "heavy_tech":
+      case 'heavy_tech':
         armyTanksEnemyStrength *= 1.5;
         break;
-      case "minefield":
+      case 'minefield':
         armyTanksStrength *= 0.7;
         armySquadsStrength *= 0.9;
         break;
-      case "patriotic":
+      case 'patriotic':
         armySquadsEnemyStrength *= 1.5;
         break;
     }
@@ -429,7 +429,7 @@ export async function showStats(id: string, enemyId: string) {
     };
     return battleStats;
   } catch (err) {
-    console.error("Database Error:", err);
+    console.error('Database Error:', err);
     throw err;
   } finally {
     client.release();
